4074ff43e6e65a055dba18eb8e665221c53209af
AbdielP <abdiel.pinzonc@gmail.com>
2021-10-08 02:03:57 -0500

    form styles and validation with javascript ready

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,8 @@
     const App = {
         htmlElements: {
             form: document.getElementById('form'),
-            input: document.getElementById('input-email')
+            input: document.getElementById('input-email'),
+            error: document.getElementById('error-message')
         },
         init: () => {
             App.bindEvents();
@@ -20,7 +21,13 @@
             validateForm: (value) => {
                 const regex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
                 const validate = regex.test(value);
-                console.log(validate);
+                if (!validate) {
+                    App.htmlElements.error.style.display = 'block';
+                    App.htmlElements.input.classList.add('footer__newsletter__input--error');
+                } else {
+                    App.htmlElements.error.style.display = 'none';
+                    App.htmlElements.input.classList.remove('footer__newsletter__input--error');
+                }
             }
         },
     }
